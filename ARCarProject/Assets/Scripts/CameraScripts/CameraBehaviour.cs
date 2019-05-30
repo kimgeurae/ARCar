9b3b115eecc9c7cfd06759a0f6fe578e69d74ff9
Kim <kimgeurae@outlook.com>
2019-05-29 21:26:16 -0300

    Update 01
    * Added example target gameobject.
    * Setup basic CameraBehaviour (Rotation and States).
    * Setup Navigation: when clicking to a face of the target gameobject the camera will look to it in a given rotation.

---
+++ ARCarProject/Assets/Scripts/CameraScripts/CameraBehaviour.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    #region Variables
+    private Transform _target;
+    public enum State {Orbit, Transition, VisualizeInfo,};
+    public State state;
+    private Vector3 rot;
+    private float sensitivity = 30f;
+    int transformIndex;
+    public Transform[] camPositions;
+    #endregion
+
+    private void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Target").transform;
+        state = State.Orbit;
+        rot = transform.localRotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void SetTargetForTransition(string t)
+    {
+        switch (t)
+        {
+            case "TopCollider":
+                transformIndex = 4;
+                break;
+            case "BotCollider":
+                transformIndex = 5;
+                break;
+            case "LeftCollider":
+                transformIndex = 2;
+                break;
+            case "RightCollider":
+                transformIndex = 3;
+                break;
+            case "FrontCollider":
+                transformIndex = 1;
+                break;
+            case "BackCollider":
+                transformIndex = 0;
+                break;
+        }
+        Debug.Log(camPositions[transformIndex].eulerAngles.ToString());
+        state = State.Transition;
+    }
+
+    private void LateUpdate()
+    {
+        BehaviourTree();  
+    }
+
+    private void BehaviourTree()
+    {
+        switch (state)
+        {
+            case State.Orbit:
+                StateOrbit();
+                break;
+            case State.Transition:
+                StateTransition();
+                break;
+            case State.VisualizeInfo:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void StateOrbit()
+    {
+        FaceTarget();
+        RotateCamera();
+        FollowTargetPosition();
+    }
+
+    private void FaceTarget()
+    {
+        transform.LookAt(_target.transform.position);
+    }
+
+    private void RotateCamera()
+    {
+        float inputX = Input.GetAxis("Vertical");
+        float inputY = Input.GetAxis("Horizontal");
+        inputY = inputY * -1;
+        rot.x += inputX * sensitivity * Time.deltaTime;
+        rot.y += inputY * sensitivity * Time.deltaTime;
+        Quaternion localRotation = Quaternion.Euler(rot.x, rot.y, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    private void FollowTargetPosition()
+    {
+        transform.position = _target.position;
+    }
+
+    private void StateTransition()
+    {
+        WarpCamera();
+    }
+
+    private void WarpCamera()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, camPositions[transformIndex].rotation, 5 * Time.deltaTime);
+        if (transform.rotation == camPositions[transformIndex].localRotation)
+            state = State.VisualizeInfo;
+        // Warp cam to desired position. Desired position would be something like: camPositions[transformIndex]
+    }
+}
---
+++ ARCarProject/Assets/Scripts/CameraScripts/OrbitNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitNavigation : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+    CameraBehaviour _cameraBehaviour;
+
+    private void Start()
+    {
+        _cameraBehaviour = GameObject.FindGameObjectWithTag("CameraRoot").GetComponent<CameraBehaviour>();
+    }
+
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _cameraBehaviour.state == CameraBehaviour.State.Orbit)
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                print(hit.collider.name);
+                _cameraBehaviour.SetTargetForTransition(hit.collider.name.ToString());
+            }
+        }
+    }
+}
